8705e2d09191d09acee0552253d81828089faf2e
James Schwartz <james@schwartz.engineer>
2016-12-11 17:30:49 -0500

    Refactor drawLines function.

--- src/App.js
+++ src/App.js
@@ -29,21 +29,19 @@ export default class App extends Component {
     const boxes = boundingBox.split(splitLine);
 
     if(direction === 'x') {
-      return (
-        <g>
-          <line key={Math.random()} x1={node.node.x} y1={boundingBox.min.y} x2={node.node.x} y2={boundingBox.max.y} strokeWidth={2} stroke={"blue"} />
-          {this.drawLines(node.left, 'y', boxes.low)}
-          {this.drawLines(node.right, 'y', boxes.high)}
-        </g>
+      this.lines.push(
+        <line key={Math.random()} x1={node.node.x} y1={boundingBox.min.y} x2={node.node.x} y2={boundingBox.max.y} strokeWidth={2} stroke={"blue"} />
       )
+
+      this.drawLines(node.left, 'y', boxes.low);
+      this.drawLines(node.right, 'y', boxes.high);
     } else {
-      return (
-        <g>
-          <line key={Math.random()} x1={boundingBox.min.x} y1={node.node.y} x2={boundingBox.max.x} y2={node.node.y} strokeWidth={2} stroke={"red"}/>
-          {this.drawLines(node.left, 'x', boxes.low)}
-          {this.drawLines(node.right, 'x', boxes.high)}
-        </g>
+      this.lines.push(
+        <line key={Math.random()} x1={boundingBox.min.x} y1={node.node.y} x2={boundingBox.max.x} y2={node.node.y} strokeWidth={2} stroke={"red"}/>
       )
+
+      this.drawLines(node.left, 'x', boxes.low);
+      this.drawLines(node.right, 'x', boxes.high);
     }
   }
 
@@ -74,20 +72,22 @@ export default class App extends Component {
   }
 
   render() {
+    this.lines = [];
+
     const output = this.points.map((point, index) => {
       return <circle key={index} cx={point.x} cy={point.y} r={3} />
     });
 
     let boundingBox = new BoundingBox(new Point(0, 0), new Point(1000, 1000));
 
-    const output2 = this.drawLines(this.tree, 'x', boundingBox);
+    this.drawLines(this.tree, 'x', boundingBox);
     const output3 = this.drawBox(this.tree, 'x', boundingBox);
 
     return (
       <div className="App">
         <SVG onClick={null}>
           {output3}
-          {output2}
+          {this.lines}
           {output}
         </SVG>
       </div>
